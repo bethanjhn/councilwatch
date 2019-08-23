ad5eb65f2c744f6513df53798cf9bf33738357ac
bethanjhn <46195797+bethanjhn@users.noreply.github.com>
2019-08-23 14:01:12 +0100

    create tweet.py
    This is the python script used to run the bot

---
+++ tweet.py
@@ -0,0 +1,20 @@
+import tweepy, time, sys
+from samplekeys import keys
+
+argfile = str(sys.argv[1])
+
+CONSUMER_KEY = keys['consumer_key']
+CONSUMER_SECRET = keys['consumer_secret']
+ACCESS_TOKEN = keys['access_token']
+ACCESS_TOKEN_SECRET = keys['access_token_secret']
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+filename=open(argfile,'r')
+f=filename.readlines()
+filename.close()
+
+for line in f:
+    api.update_status(status=line)
+    time.sleep(14400)#Tweet four times a day
